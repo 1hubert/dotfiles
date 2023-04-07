e2ca907e994e8b45544648232743bce94a2628d2
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2023-04-07 17:00:50 +0200

    sxhkd: delete launch.sh
    I'll stick to `pkill -USR1 -x sxhkd` for reloading sxhkd configuration

--- .config/sxhkd/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# Terminate already running sxhkd
-
-# Wait until the processes have been shut down
-while pgrep -x sxhkd >/dev/null; do sleep 1; done
-
-# Launch sxhkd
-sxhkd -c /home/$USER/.config/sxhkd/sxhkdrc &
